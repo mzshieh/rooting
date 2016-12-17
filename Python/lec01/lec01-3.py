f1aeda6078067ed68311a9e4a8a993fcdd008e52
Min-Zheng Shieh <mzshieh@nctu.edu.tw>
2016-12-17 07:08:49 +0000

    sample code

---
+++ Python/lec01/lec01-3.py
@@ -0,0 +1,13 @@
+place = input('Where are you from? ')
+if place == 'Taiwan':
+    print('吃飽沒')
+    food = input('What\'s your favorite food? ')
+    if food == 'Pineapple':
+        print('Me, too.')
+    else:
+        print('I love pineapple')
+        print('But I\'ll try '+food)
+else:
+    print('Hi')
+
+    
---
+++ Python/lec01/lec01-4.py
@@ -0,0 +1,8 @@
+grade = input('你幾年級? ')
+if grade == '1':
+    print('你好年輕')
+elif grade == '3':
+    print('你老了')
+else:
+    print('跟我一樣')
+
